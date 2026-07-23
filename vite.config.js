eaa2e9b15b0ec7f3e5fb47c7a306ea6d895aa1b1
Sushruth Hebbar <kalyani.mvr1@gmail.com>
2026-07-24 00:32:35 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Plantationservices.ai/', // IMPORTANT
-
-  server: {
-    port: 3000,
-    open: true
-  },
-
-  build: {
-    outDir: 'dist',
-    sourcemap: false
-  }
+  base: '/Plantationservices.ai/',
 })
